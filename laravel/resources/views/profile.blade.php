f51c631f5c0d60df567d94dc28f2067fe862bdad
hehe <Yanis.monnet1@gmail.com>
2021-03-25 22:53:14 +0100

    add FRONT for register.blade.php and update.blade.php

---
+++ laravel/resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="Profile.css" media="all"/>
+</head>
+<body style="font-family: Nunito">
+<div class="information" style="text-align: center">
+    <h1>Your informations :</h1>
+    <p>Last-name : </p>
+    <p>First-name : </p>
+    <p>Account permission : </p>
+    <p>internship offer : </p>
+</div>
+<div class="boutton" style="text-align: center; margin-top: 25%">
+    <input type="submit" style="background-color: #d43f3a" value="Delete">
+    <input type="submit" style="background-color: #f0ad4e" value="Edit">
+</div>
+</body>
+</html>
