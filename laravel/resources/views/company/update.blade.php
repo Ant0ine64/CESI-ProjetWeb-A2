8211b0db3891f104af25e3e1baa5ceef948133ff
Fudjia <seraphin2000@live.Fr>
2021-03-25 20:36:49 +0100

    Dirty code cleaned

--- laravel/resources/views/company/update.blade.php
+++ laravel/resources/views/company/update.blade.php
@@ -4,16 +4,13 @@
     <meta charset="UTF-8">
     <title>UpdateCompany</title>
 </head>
-
-
-
-<!-- This code is really dirty .. but works -->
-       <?php
-       use App\Http\Controllers\CompanyController;if(!isset($_GET['id'])){
-               echo "Wrong usage !";
-               return;
+<body>
+    <?php
+           use App\Http\Controllers\CompanyController;
+           if(!isset($_GET['id'])){
+                   echo "Wrong usage !";
+                   return;
            }
-
 
            $companyId = $_GET['id'];
            $companyInfos = CompanyController::tryGettingCompany($companyId)->First();
@@ -22,27 +19,21 @@
                echo "This company doesn't exists !";
                return;
            }
-
-       ?>
-               <form action="{{route('company.create')}}" method="post"/>
+    ?>
+               <form action="{{route('company.update')}}" method="post">
                @csrf
-
-
-       <?php
-               echo "Company id :";
-               echo "<input value=\"{$companyInfos->id}\" type=\"text\" name=\"companyId\" id=\"companyId\" readonly><br>";
-               echo "Company name :";
-               echo "<input value=\"{$companyInfos->name}\" type=\"text\" name=\"name\" id=\"name\"><br>";
-               echo "Company address :";
-               echo "<input value=\"{$companyInfos->address}\" type=\"text\" name=\"address\" id=\"address\"><br>";
-               echo "Company activity sector :";
-               echo "<input value=\"{$companyInfos->activity_sector}\" type=\"text\" name=\"activitySector\" id=\"activitySector\"><br>";
-               echo "Company interns number :";
-               echo "<input value=\"{$companyInfos->interns_number}\" type=\"text\" name=\"internsNumber\" id=\"internsNumber\"><br>";
-               echo "<input type=\"submit\" value=\"UpdateCompany\">";
-               echo "</form>";
-       ?>
-
+               Company id :
+               <input value="<?php echo $companyInfos->id; ?>" type="text" name="companyId" id="companyId" readonly><br>
+               Company name :
+               <input value="<?php echo $companyInfos->name; ?>" type="text" name="name" id="name"><br>
+               Company address :
+               <input value="<?php echo $companyInfos->address; ?>" type="text" name="address" id="address"><br>
+               Company activity sector :
+               <input value="<?php echo $companyInfos->activity_sector; ?>" type="text" name="activitySector" id="activitySector"><br>
+               Company interns number :
+               <input value="<?php echo $companyInfos->interns_number; ?>" type="text" name="internsNumber" id="internsNumber"><br>
+               <input type="submit" value="UpdateCompany">
+               </form>
 </body>
 </html>
 
