1c251fb653e300ddea590ffc5bbdc63880399def
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2021-03-25 15:07:47 +0100

    Radio boxes keep their value

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -11,22 +11,27 @@ use App\Models\Company;
 use App\Models\Offer;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\Console\Input\Input;
+
+use function Psy\debug;
 
 class SearchController extends Controller
 {
     //READ
-    static function readAll() {
+    static function readAll(Request $request) {
+        $radio = $request->get('filter');
         $users = User::join('center', 'user.id_center', '=', 'center.id')->join('type', 'user.id_type', '=', 'type.id')->get();
         $offers = Offer::join('company', 'offer.id_company', '=', 'company.id')->get();
         $comps = Company::get();
-        return view('search', ['users' => $users, 'offers' => $offers, 'comps' => $comps]);
+        return view('search', ['users' => $users, 'offers' => $offers, 'comps' => $comps, 'radio' => $radio]);
     }
 
     //READ
-    static function readAllG() {
+    static function readAllG(Request $request) {
+        $radio = $request->get('filter');
         $users = User::join('center', 'user.id_center', '=', 'center.id')->join('type', 'user.id_type', '=', 'type.id')->get();
         $offers = Offer::join('company', 'offer.id_company', '=', 'company.id')->get();
         $comps = Company::get();
-        return view('gestion', ['users' => $users, 'offers' => $offers, 'comps' => $comps]);
+        return view('gestion', ['users' => $users, 'offers' => $offers, 'comps' => $comps, 'radio' => $radio]);
     }
 }
