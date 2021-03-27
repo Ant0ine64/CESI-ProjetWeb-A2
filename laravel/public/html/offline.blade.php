c7c8228b5641c6b9256a51463303f31e8961e861
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2021-03-27 14:00:33 +0100

    PWA working online, offline page not done yet

---
+++ laravel/public/html/offline.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <link href="/css/offline.css" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#000000">
+        <title>Offline</title>
+        <link rel="manifest" href="manifest.json">
+        <link rel="apple-touch-icon" href="img/cesinkdin">
+    </head>
+    <body>
+            <p>You are offline</p>
+            <script src="index.js"></script>
+    </body>
+</html>
--- laravel/resources/views/login.blade.php
+++ laravel/resources/views/login.blade.php
@@ -3,7 +3,11 @@
 <head>
         <link href="/css/login.css" rel="stylesheet">
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#000000">
         <title>Login</title>
+        <link rel="manifest" href="manifest.json">
+        <link rel="apple-touch-icon" href="img/cesinkdin">
     </head>
     <body>
             <div id="log_form_div">
@@ -19,5 +23,6 @@
                     <a href="ask_account">No account yet ?</a>
                 </form>
             </div>
+            <script src="index.js"></script>
     </body>
 </html>
