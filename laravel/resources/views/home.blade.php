02d2c853e2ab10b8c1cb36041a69ba8e0f3a6148
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2021-03-29 21:37:40 +0200

    Fire commit 2

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -10,18 +10,8 @@
             @include ('header')
         </header>
             <main>
-<<<<<<< HEAD
                 <section class="sect-left">
                     
-=======
-                <section id="sect-left">
-                    <p>
-
-                    </p>
-                </section>
-                <section id="sect-mid">
-
->>>>>>> 0891dd0cf01eb344c090abcb3d7d112aa69bdf70
                 </section>
                 <section class="sect-middle">
 
@@ -38,7 +28,6 @@
                     <th>Actions</th>
                 </tr>
                 @foreach ($wishes as $wish)
-<<<<<<< HEAD
                 <tr>
                     <td>{{$wish->Name}}</td>
                     <td>{{$wish->title}}</td>
@@ -52,20 +41,6 @@
                     @endif
                     </td>
                 </tr>
-=======
-                    <tr>
-                        <td>{{$wish->name}}</td>
-                        <td>{{$wish->title}}</td>
-                        <td>{{$wish->date}}</td>
-                        <td>{{$wish->duration}}</td>
-                        <td>{{$wish->state}}</td>
-                        <td>
-                        @if (\App\Http\Controllers\PermissionController::can('wishlist.remove'))
-                            <a href="#" class="clickme danger">Delete</a>
-                        @endif
-                        </td>
-                    </tr>
->>>>>>> 0891dd0cf01eb344c090abcb3d7d112aa69bdf70
                 @endforeach
                 </table>
                 </section>
