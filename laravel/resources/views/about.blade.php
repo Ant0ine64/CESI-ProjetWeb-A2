015c01fbda29c342d8e39772dd687a29be13df6c
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2021-03-29 15:13:27 +0200

    Add about page

---
+++ laravel/resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/css/about.css">
+</head>
+<header>
+    @include ('header');
+</header>
+<body>
+    <main><p>This website was made so that any CESI student could find an internship easily.</p> </main>
+</body>
+</html>
