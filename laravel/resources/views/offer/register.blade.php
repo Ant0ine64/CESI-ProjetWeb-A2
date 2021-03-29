cad4759693c2f32002e69505a931f5d91fd3e5df
Antoine <contact@ant0ine64.fr>
2021-03-29 14:48:50 +0200

    Add offer view & button (maybe broken)

--- laravel/resources/views/offer/register.blade.php
+++ laravel/resources/views/offer/register.blade.php
@@ -10,23 +10,27 @@
     <header>
         @include ('header')
     </header>
-    <form action="{{route('offer.create')}}" method="post" class="form">
+    <form action="{{route('offer.create')}}" method="post" class="form" style="text-align: center;">
         @csrf
-        <label for="idCompany">Company ID : </label><br>
-        <input value = "23" type="text" name="idCompany" id="idCompany"><br>
-        <label for="title">Company title : </label><br>
-        <input value = "defaultValue" type="text" name="title" id="title"><br>
-        <label for="competences">Compétences :</label><br>
-        <input value = "defaultValue"type="text" name="competences" id="competences"><br>
-        <label for="date">Date 1 [A redéfinir] : </label><br>
-        <input type="date" name="date" id="date" value="2021-03-22" min="2021-03-22"><br> <!--todo: faire en sorte de display a partir de la date actuelle -->
+        <label for="idCompany">Company Name : </label><br>
+        <input type="text" name="idCompany" id="idCompany"><br>
+        <label for="title">Title : </label><br>
+        <input type="text" name="title" id="title"><br>
+        <label for="competences">Skills :</label><br>
+        <input type="text" name="competences" id="competences"><br>
+        <label for="date">Start Date : </label><br>
+        <input type="date" name="date" id="date" min="2021-03-22"><br> <!--todo: faire en sorte de display a partir de la date actuelle -->
         <label for="endDate">End date : </label><br>
-        <input type="date" name="endDate" id="endDate" value="2021-03-23" min="2021-03-22"><br> <!--todo: min doit etre la date de selectionnée dans date (juste au dessus) -->
+        <input type="date" name="endDate" id="endDate"  min="2021-03-22"><br> <!--todo: min doit etre la date de selectionnée dans date (juste au dessus) -->
         <label for="remuneration">Salary : </label><br>
-        <input value = "535.6" type="text" name="remuneration" id="remuneration"><br>
+        <input type="text" name="remuneration" id="remuneration"><br>
         <label for="slots">Slots : </label><br>
-        <input value = "12" type="text" name="slots" id="slots"><br><br>
-        <input type="submit" value="RegisterOffer" style="padding: 1%; border-radius:10%; margin-left: 10px">
+        <input type="text" name="slots" id="slots"><br><br>
+        <input type="submit" value="Add this offer" style="
+        padding: 8px 20px; font-weight: bold; border-radius: 5px; cursor: pointer;
+        background-color: green;
+        color: #FFFFFF;
+        ">
     </form>
     <footer>
         @include('footer')
