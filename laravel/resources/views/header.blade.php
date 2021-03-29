ca684defac20a5a07b2bc538de35c45c52a9fd64
hehe <Yanis.monnet1@gmail.com>
2021-03-29 14:00:23 +0200

    Changement franglais à anglais

--- laravel/resources/views/header.blade.php
+++ laravel/resources/views/header.blade.php
@@ -12,7 +12,7 @@
           <a href="profile?id={{Auth::user()->id}}" id="Profile">Profile</a>
         </div>
         <div class="part2">
-            <a href="logout" id="Deco">Déconnexion</a>
+            <a href="logout" id="Deco">Disconnect</a>
         </div>
         <div class="part_">
             <img src="img/cesinkdin.png" alt="logo" style="float: right" id="logo">
