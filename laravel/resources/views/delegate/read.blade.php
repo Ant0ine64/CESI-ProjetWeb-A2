4c18956dacbeb0a05c49a334be6293dc052dcc2f
hehe <Yanis.monnet1@gmail.com>
2021-03-30 14:37:03 +0200

    css

--- laravel/resources/views/delegate/read.blade.php
+++ laravel/resources/views/delegate/read.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link href="/css/search.css" rel="stylesheet">
+    <link href="/css/read.css" rel="stylesheet">
     <meta charset="UTF-8">
     <title>Delegue</title>
 </head>
@@ -9,41 +9,47 @@
 <header id="header">
     @include ('header')
 </header>
-    <form action="{{route('delegate.read')}}" method="get">
-        <input type="text" name="login" id="login"><br>
-        <input type="submit" value="Search delegate"><br>
-    </form>
-    @isset($username)
-        <p>Permission of : {{$username}} :</p><br>
-    @isset($user_permissions)
-        @php($permissions = \App\Http\Controllers\PermissionController::readAllDelegablePermissions())
-        <form action="{{route('delegate.update')}}" method="post">
-            <table>
-                <thead>
-                <tr>
-                    <th colspan="3">Permissions</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($permissions as $permission => $title)
-                <tr>
-                    <td>{{$permission}}</td>
-                    <td>{{$title}}</td>
-                    @if(in_array($permission, $user_permissions))
-                        <td><input type="checkbox" checked="enabled" name="{{$permission}}"></td>
-                    @else
-                        <td><input type="checkbox" name="{{$permission}}"></td>
-                    @endif
-                </tr>
-                @endforeach
-                </tbody>
-            </table>
-            <input type="hidden" name="login" value="{{$username}}">
-            <input type="submit" value="Save">
+<main style="height: 130%">
+    <div>
+        <form action="{{route('delegate.read')}}" method="get">
+            <input type="text" name="login" id="login"><br>
+            <input type="submit" value="Search delegate"><br>
         </form>
+    </div>
+    <div>
+        @isset($username)
+            <p>Permission of : {{$username}} :</p><br>
+        @isset($user_permissions)
+            @php($permissions = \App\Http\Controllers\PermissionController::readAllDelegablePermissions())
+            <form action="{{route('delegate.update')}}" method="post">
+                <table>
+                    <thead>
+                    <tr>
+                        <th colspan="3">Permissions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($permissions as $permission => $title)
+                    <tr>
+                        <td>{{$permission}}</td>
+                        <td>{{$title}}</td>
+                        @if(in_array($permission, $user_permissions))
+                            <td><input type="checkbox" checked="enabled" name="{{$permission}}"></td>
+                        @else
+                            <td><input type="checkbox" name="{{$permission}}"></td>
+                        @endif
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <input type="hidden" name="login" value="{{$username}}">
+                <input type="submit" value="Save">
+            </form>
+    </div>
     @endisset
     @endisset
-<footer>
+</main>
+<footer id="footer">
     @include('footer')
 </footer>
 </body>
