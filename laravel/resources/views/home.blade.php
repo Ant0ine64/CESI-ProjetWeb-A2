9248d1ae93ec4f32ff59bda6d8ca43bedd976e25
BenoitCesi <benoit.desbourdieux@viacesi.fr>
2021-04-01 10:14:42 +0200

    Changed routes so it works

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
         <link href="/css/home.css" rel="stylesheet">
+        <link rel="stylesheet" href="/css/headerFooter.css">
         <meta charset="UTF-8">
         <title>Home</title>
 </head>
@@ -10,7 +11,7 @@
             @include ('header')
         </header>
             <main id="gueule_de_benoit">
-                <img src="/img/IMG_20210331_094448.jpg" alt="Photo Benoît CESI"class="gueule_de_benoit">
+                <img src="/img/IMG_20210331_094448.jpg" alt="Photo Benoît CESI" class="img_home">
             </main>
         <footer>
             @include ('footer')
